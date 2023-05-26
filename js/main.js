8a46af34ed58e0ce030bd3729fe46db7b75ca4b5
nikitazu20 <121955569+nikitazu20@users.noreply.github.com>
2023-05-26 14:02:32 +0000

    Deploying to gh-pages from @ nikitazu20/2170671-cat-energy-28@d3486fdb20a63de609ad673ed376e639045a813c 🚀

---
+++ js/main.js
@@ -0,0 +1,1 @@
+let navMain=document.querySelector(".navigation"),navToggle=document.querySelector(".navigation__toggle");navMain.classList.remove("navigation--nojs"),navMain.classList.add("navigation--closed"),navMain.classList.remove("navigation--opened"),navToggle.addEventListener("click",(()=>{navMain.classList.toggle("navigation--closed"),navMain.classList.toggle("navigation--opened")}));
